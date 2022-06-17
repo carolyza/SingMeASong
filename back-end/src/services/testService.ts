6ebba5c34bec3c3efa644ceaccb3ae8671f2e085
Liaess <yann.melo@gmail.com>
2022-06-17 11:50:57 -0300

    Preparando revisão de código

--- back-end/src/services/testService.ts
+++
@@ -1,7 +0,0 @@
-import { recommendationRepository } from "../repositories/recommendationRepository.js";
-import { faker } from "@faker-js/faker";
-import { CreateRecommendationData } from "./recommendationsService.js";
-
-export async function resetDatabase() {
-  await recommendationRepository.truncate();
-}
